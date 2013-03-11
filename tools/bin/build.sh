5ee37781e657974d69e44a806d2ca25e6463ea0e
Jan Vermeir <jvermeir@xebia.com>
2013-03-11 17:54:57 +0100

    generic version of build.sh replaces specific versions in other projects.

--- tools/bin/build.sh
+++ tools/bin/build.sh
@@ -3,7 +3,7 @@
 APP=$1
 echo "Generating deployment archive for '$APP'"
 
-DEPLOYIT_HOME=/Users/jan/dev/blogs/deployitBlogs/deployit-3.7.4-server
+DEPLOYIT_HOME=/Users/jan/dev/blogs/deployitBlogs/deployit-3.8.4-server
 
 TIMESTAMP=`date +"%Y%m%d-%H%M%S"`
 TMP=target/dar
@@ -13,12 +13,13 @@ fi
 
 mkdir -p $TMP/META-INF
 cat src/main/resources/manifest.template | sed "s/<application>/$APP/g;s/<timestamp>/$TIMESTAMP/g" > $TMP/META-INF/MANIFEST.MF
-mkdir -p $TMP/resources
-echo $TIMESTAMP > $TMP/resources/$TIMESTAMP.txt
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" > $TMP/someFile.txt
+echo $TIMESTAMP >> $TMP/someFile.txt
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" >> $TMP/someFile.txt
 cd $TMP
 rm ../$APP.dar
 zip -r ../$APP.dar *
 cd -
 cp target/$APP.dar $DEPLOYIT_HOME/importablePackages
 
-echo "'$APP' copied, start Deploy gui and import"
+echo "'$APP' copied to importablePackages directory, start Deploy gui and import"
