ce2be5e98435a77cdd72c84e2eb455054c287972
Jan Vermeir <jvermeir@xebia.com>
2013-03-25 21:16:59 +0100

    Fixed copy path problems and resources directory in dar file.

--- tools/bin/build.sh
+++ tools/bin/build.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+CURR=`PWD`
 APP=$1
 echo "Generating deployment archive for '$APP'"
 
@@ -13,13 +14,12 @@ fi
 
 mkdir -p $TMP/META-INF
 cat src/main/resources/manifest.template | sed "s/<application>/$APP/g;s/<timestamp>/$TIMESTAMP/g" > $TMP/META-INF/MANIFEST.MF
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" > $TMP/someFile.txt
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" > $TMP/resources/someFile.txt
 echo $TIMESTAMP >> $TMP/someFile.txt
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" >> $TMP/someFile.txt
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" >> $TMP/resources/someFile.txt
 cd $TMP
 rm ../$APP.dar
 zip -r ../$APP.dar *
-cd -
-cp target/$APP.dar $DEPLOYIT_HOME/importablePackages
+cd $CURR
 
-echo "'$APP' copied to importablePackages directory, start Deploy gui and import"
+$DEPLOYIT_HOME/deployit-3.8.4-cli/bin/cli.sh -username admin -password admin -f deploy.cli -- $CURR/target/$APP.dar localenv
